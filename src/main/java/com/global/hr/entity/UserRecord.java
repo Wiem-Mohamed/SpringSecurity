404439efff1d9949caa27175f40a4f53f2b9fab6
Wiem Mohamed <107613285+Wiem-Mohamed@users.noreply.github.com>
2023-10-29 21:22:31 +0100

    Update UserRecord.java

--- src/main/java/com/global/hr/entity/UserRecord.java
+++ src/main/java/com/global/hr/entity/UserRecord.java
@@ -1,3 +1,2 @@
 package com.global.hr.entity;
-//estaamelt record lenna bech nreturni juste hajet par exemple lpassword la
 public record UserRecord(Long id, String firstName, String lastName, String email){}
